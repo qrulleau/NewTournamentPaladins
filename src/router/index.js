768d26aa709058c224a34daec5cb5343dc727463
qrulleau <quentin.rulleau@yahoo.com>
2021-05-09 20:33:02 +0200

    ADD: set up all route for the navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
-import HomePage from '../views/HomePage/HomePage.vue';
-import ContactPage from '../views/ContactPage/ContactPage.vue';
+import HomePage from '../views/Home/HomePage.vue';
+import Calendar from '../views/Calendar/CalendarPage.vue';
+import Result from '../views/Result/ResultPage.vue';
+import Team from '../views/Team/TeamPage.vue';
+import ContactPage from '../views/Contact/ContactPage.vue';
 //import AppError from '../views/AppError.vue';
 
 const routes = [
@@ -9,6 +12,18 @@ const routes = [
 		path: '/',
 		name: 'Home',
 		component: HomePage,
+	},
+	{
+		path: '/calendrier',
+		component: Calendar,
+	},
+	{
+		path: '/resultat',
+		component: Result,
+	},
+	{
+		path: '/equipes',
+		component: Team,
 	},
 	{
 		path: '/contact-page',
