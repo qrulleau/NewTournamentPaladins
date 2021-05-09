a4503a27ccd9b7284713dac534d2ec400a728afe
qrulleau <quentin.rulleau@yahoo.com>
2021-05-09 17:04:12 +0200

    ADD: creation of header and footer component

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,53 @@
+<template>
+	<footer>
+		<div class="background-black white">
+			<div class="container">
+				<div class="d-flex item-align-center">
+					<div>
+						<h1>Logo<span>Tournois</span></h1>
+					</div>
+					<div class="d-flex">
+						<div class="row">
+							<h4>Lien du site</h4>
+							<router-link to="/contact">Contact</router-link>
+							<router-link to="/home">Home</router-link>
+							<router-link to="">Reglement</router-link>
+						</div>
+						<div class="row">
+							<h4>Tournois & Equipes</h4>
+							<router-link to="/calendar">Calendrier</router-link>
+							<router-link to="/team">Equipes</router-link>
+							<router-link to="/result">Résultat</router-link>
+						</div>
+						<div class="row">
+							<h4>Légales</h4>
+							<router-link to="">Politique de confidentialité</router-link>
+							<router-link to="">Politique de cookie</router-link>
+							<router-link to="">Gestion des cookies</router-link>
+						</div>
+						<div class="row">
+							<h4>à propos</h4>
+							<p>
+								Le NomTournois est basé en France, crée par des joueurs pour des joueurs, à l’écoute de la communauté Afin de faire perduré Paladins !
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="d-flex copyright">
+					<p>Copyright 2021 NomTournois. Tous droits réservés</p>
+					<p>Design & developed by quentin Rulleau</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.row a {
+	display: block;
+}
+</style>
---
+++ src/components/layout/TheNavBar.vue
@@ -0,0 +1,33 @@
+<template>
+	<header>
+		<div class="container">
+			<div class="d-flex">
+				<div>
+					<h1 class="white">logo<span>Tournois</span></h1>
+				</div>
+				<nav>
+					<div class="d-flex">
+						<router-link to="/">Home</router-link>
+						<router-link to="/calendar">Calendrier</router-link>
+						<router-link to="/resultat">Résultat</router-link>
+						<router-link to="/team">Equipes</router-link>
+						<router-link to="/contact">Contact</router-link>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+span {
+	font-weight: bold;
+}
+a {
+	margin-left: 44px;
+}
+</style>
