9ceb9609d806804dac48b36f64ee8beb61a69bbf
qrulleau <quentin.rulleau@yahoo.com>
2021-06-14 16:37:25 +0200

    ADD: Hamburger navigation with responsive design

--- src/components/layout/TheNavBar.vue
+++ src/components/layout/TheNavBar.vue
@@ -3,11 +3,14 @@
 		<div class="container">
 			<div class="d-flex item-align-center">
 				<div>
-					<h1>
+					<h1 class="desktop">
 						<img src="https://i.ibb.co/3B7wVMh/logo-Tournament.png" alt="" />
 					</h1>
+					<h1 class="mobile">
+						<img src="../../assets/LogoTournament/logoNoText.png" alt="" />
+					</h1>
 				</div>
-				<nav>
+				<nav class="nav">
 					<div class="d-flex">
 						<router-link to="/">Home</router-link>
 						<router-link to="/calendrier">Calendrier</router-link>
@@ -16,13 +19,31 @@
 						<router-link to="/contact">Contact</router-link>
 					</div>
 				</nav>
+				<button class="hamburger hamburger--spring" type="button" :class="{ 'is-active': isActive }" @click="test()">
+					<span class="hamburger-box">
+						<span class="hamburger-inner"></span>
+					</span>
+				</button>
 			</div>
 		</div>
 	</header>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			isActive: false,
+		};
+	},
+	methods: {
+		test() {
+			this.isActive = !this.isActive;
+			const Nav = document.querySelector('nav');
+			Nav.classList.toggle('displayMenu');
+		},
+	},
+};
 </script>
 
 <style lang="scss" scoped>
