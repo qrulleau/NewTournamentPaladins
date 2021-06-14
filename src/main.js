81a503c7a16a07ad67470214550a857e078e9211
qrulleau <quentin.rulleau@yahoo.com>
2021-06-14 16:29:29 +0200

    ADD: global scss file

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import './index.scss';
+import './hamburger.scss';
+
 const app = createApp(App);
 app.use(router).mount('#app');
