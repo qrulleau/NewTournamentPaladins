c98fcba84cc2cc1a827d21dcbbfaf502100b97fe
qrulleau <quentin.rulleau@yahoo.com>
2021-06-18 22:57:44 +0200

    MIN: fail link for deployement

--- src/views/MatchDetail/firstTour/MinervaVsAbdul.vue
+++ src/views/MatchDetail/firstTour/MinervaVsAbdul.vue
@@ -154,7 +154,7 @@
 							</div>
 							<div class="player-profil">
 								<div class="position-relative">
-									<img src="../../../assets/ProfilPlayer/inconnu.jpg" alt="" />
+									<img src="../../../assets/ProfilPlayer/Abdul/flashou.jpg" alt="" />
 									<div class="position-absolute">
 										<h4>FLAASHOU</h4>
 										<p class="red">DPS</p>
